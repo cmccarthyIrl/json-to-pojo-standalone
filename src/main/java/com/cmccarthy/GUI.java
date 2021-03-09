a3936915eb7a9746bc874737c9f61c2f1b692baa
cmccarthyirl <craig.mccarthy@live.ie>
2021-03-09 10:29:06 +0000

    removed temp files and directories

--- src/main/java/com/cmccarthy/GUI.java
+++ src/main/java/com/cmccarthy/GUI.java
@@ -1,18 +1,22 @@
 package com.cmccarthy;
 
 import com.cmccarthy.controller.ScreenController;
+import com.cmccarthy.utils.Constants;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.io.IOException;
 
 public class GUI extends Application {
 
     public static void main(String[] args) {
         launch(args);
     }
-
     @Override
     public void start(Stage primaryStage) throws Exception {
 
@@ -25,5 +29,10 @@ public class GUI extends Application {
         primaryStage.setTitle("JSON 2 POJO");
         primaryStage.setResizable(false);
         primaryStage.show();
+    }
+
+    @Override
+    public void stop() throws IOException {
+        FileUtils.deleteDirectory(new File(Constants.outputDirectory + "/"));
     }
 }
